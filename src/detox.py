5df5e53077807ab2f19115960f3b162494b6f70c
S L E E P Y <38892760+sleepytw@users.noreply.github.com>
2021-08-25 04:07:21 +0300

    Update detox.py

--- src/detox.py
+++ src/detox.py
@@ -26,7 +26,7 @@ cV=list(reg.values()) #cache values
 for bit in reg:
     mem.append('0x-%s' % reg[bit])
 
-class Initials:
+class Build:
     def __init__(self, eax, ebx):
         self.eax = eax
         self.ebx = ebx
@@ -37,28 +37,33 @@ class Initials:
     def pop(self, pAddr): #pAddr is the cell you want you to pop
         if pAddr < 11:
             p1=mem[pAddr][:5]; p1=p1.replace('0x-', ''); p2=mem[pAddr][12:]; p2=p2.replace('0x-', ''); p1=int(p1); p2=int(p2)
-            print(p1, p2, type(p1), type(p2), '\n')
             mem.remove(mem[pAddr]); mem.remove(mem[p2-1]); mem.insert(p1-2, '0x-%s' % p1); mem.insert(p2-1, '0x-%s' % p2)
         else:
             p1=mem[pAddr][:4]; p1=p1.replace('0x-', ''); p2=mem[pAddr][12:]; p2=p2.replace('0x-', ''); p1=cK[cV.index(p1)]; p2=cK[cV.index(p2)]
-            print(p1, p2, type(p1), type(p2), '\n')
             mem.remove(mem[pAddr]); mem.remove(mem[p2-1]); mem.insert(p1-2, '0x-%s' % reg[p1]); mem.insert(p2-1, '0x-%s' % reg[p2])
 
-asm = Initials(1, 14)
+class Interpreter:
+    def __init__(self):
+        pass
 
-for pos in mem:
-    print('%s' % pos)
-print('--------------------\n\n')
+    def read(self, bL: list):
+        with open('output.txt', 'r', encoding='utf-8') as ot:
+            data = ot.readline()
+            for ln in data:
+                bL.append(ln)
 
-asm.mov()
-
-for pos in mem:
-    print('%s' % pos)
-print('--------------------\n\n')
-
-asm.pop(14)
-
-for pos in mem:
-    print('%s' % pos)
-print('--------------------\n\n')
-input()
+    def calc(self, total=0):
+        global elm
+        bls = bL[0]; rev=list(); cld=dict(); calcD = dict()
+        for evr in bls:
+            rev.append(str(evr))
+        for elm in range(len(rev)):
+            if rev[elm] == '0' or '-': cld[elm]='2'
+            elif rev[elm] == '1': cld[elm]='4'
+            else: pass
+        cV2=list(int(cld.values()))
+        for ev in range(len(cV2)):
+            calcD[ev]=cV2[ev]**(ev-1)
+        cV3=list(int(calcD.values()))
+        for ite in cV3:
+            total+=(cV3[ite]/2); total/=16
